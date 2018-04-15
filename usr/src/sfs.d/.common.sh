b0976fb3e714b9830df6a5972a151f6237dcabf1
Lauri Korts-Pärn <lauri@korc.jp>
2018-04-15 00:20:17 -0400

    use .common.sh in build scripts, add .check-up-to-date

---
+++ usr/src/sfs.d/.common.sh
@@ -0,0 +1,19 @@
+: "${lbu:=/opt/LiveBootUtils}"
+. "$lbu/scripts/common.func"
+
+: "${dist:=$(lsb_release -cs)}"
+: "${dist_arch:=$(dpkg --print-architecture)}"
+
+: "${wine_pkg:=winehq-stable}"
+: "${i386_pkgs:=libgl1-mesa-dri:i386 libtxc-dxtn-s2tc:i386}"
+
+: "${deb_repo:=https://dl.winehq.org/wine-builds/debian/}"
+: "${repo_key_url:=https://dl.winehq.org/wine-builds/Release.key}"
+
+installed_ver() {
+  grep "^Version: " "$DESTDIR/var/lib/dpkg/info/${wine_pkg}.control" | cut -f2 -d" "
+}
+
+latest_ver() {
+  curl -s "${deb_repo%/}/dists/${dist}/main/binary-${dist_arch}/Packages" | grep -e ^Package: -e ^Version: | grep -A1 -xF "Package: $wine_pkg" | grep ^Version | tail -1 | cut -f2 -d" "
+}
